cf2f7ade86f71ffa7202369012382f3ed93428c8
Zack Kollar <rallokkcaz@gmail.com>
2019-08-08 21:02:52 -0700

    Simple http server example

---
+++ Node/examples/node-http-example/index.js
@@ -0,0 +1,26 @@
+const { createServer } = require('http');
+
+const router = {
+  "/": (req, res) => {
+    res.write("Welcome!");
+  },
+  "/info": (req, res) => {
+    res.write("This is a Node HTTP Server!")
+  },
+}
+
+const server = createServer((req, res) => {
+  console.log(`${req.url} ${req.method}`)
+  
+  if(router[req.url]) {
+    res.writeHead(200, {'Content-Type': 'text/plain', 'David-Header': 'Hello David'});
+    router[req.url](req, res)
+  } else {
+    res.writeHead(404, {'Content-Type': 'text/plain'});
+    res.write(`${req.url} Not Found`);
+  }
+
+  res.end();
+});
+
+server.listen(4200);
