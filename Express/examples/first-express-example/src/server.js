074d8957f88ccc956ac7ee677d0e65f8191b30c8
Zack Kollar <rallokkcaz@gmail.com>
2019-08-09 19:52:06 -0700

    Added initial express example

---
+++ Express/examples/first-express-example/src/server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const cors = require("cors");
+
+const app = express();
+
+// Parses JSON data bodies
+app.use(express.json());
+// Added request logging
+app.use(morgan("tiny"));
+// Allows for CORS
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send({class: "Advanced Bowling!"});
+});
+
+app.post('/', (req, res) => {
+  console.log(req.body.Hello);
+
+  res.send(req.body);
+});
+
+const startServer = async () => {
+  try {
+    await mongoose.connect("mongodb://localhost/first-express-example", {
+      useNewUrlParser: true, // Avoid annoying deprecation error!
+    })
+    console.log("Connected to DB...");
+  } catch(err) {
+    console.error(`Failed to connect to MongoDB: ${err}`);
+    // Tells other programs that this failed
+    process.exit(-1);
+  }
+
+  app.listen(8000, () => {
+    console.log("Listening at localhost:8000...");
+  });
+}
+
+startServer();
